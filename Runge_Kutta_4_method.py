622501efa56b3125a60708131b62d7b8d60dba50
abdelrahmanmagdy7077 <abdelrahman.abouzied7707@proton.me>
2026-07-07 00:21:32 +0300

    Renamed RK4 file & improved code readability

---
+++ Runge_Kutta_4_method.py
@@ -0,0 +1,33 @@
+def runge_kutta_4(derivative, t_start, t_end, y_initial, step_size):
+
+    time_points = [t_start]
+    solution_points = [y_initial]
+
+    #idk if that will fix floating point problems
+    num_steps = int( round( (t_end - t_start)  / step_size ) )
+
+    t_current = t_start
+    y_current = y_initial
+
+    for _ in range(num_steps):
+        k1 = derivative(t_current, y_current)
+        k2 = derivative(t_current + step_size / 2, y_current + (step_size / 2 * k1))
+        k3 = derivative(t_current + step_size / 2, y_current + (step_size / 2 * k2))
+        k4 = derivative(t_current + step_size, y_current + (step_size * k3))
+
+        average_of_derivatives = step_size * 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+
+        y_next = y_current + average_of_derivatives
+        t_next = t_current + step_size
+
+        time_points.append(t_next)
+        solution_points.append(y_next)
+
+        t_current = t_next
+        y_current = y_next
+
+    return time_points, solution_points
+
+
+
+
