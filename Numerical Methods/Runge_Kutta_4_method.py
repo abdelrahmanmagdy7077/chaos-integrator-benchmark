992102c72e31b253cefa077a6b25d4cad72baf60
abdelrahmanmagdy7077 <abdelrahman.abouzied7707@proton.me>
2026-07-09 19:17:39 +0300

    Changed the code so it works with numpy arrays instead of singular variable/functions

--- Numerical Methods/Runge_Kutta_4_method.py
+++ Numerical Methods/Runge_Kutta_4_method.py
@@ -1,12 +1,18 @@
+import numpy as np
+
 def runge_kutta_4(derivative, t_start, t_end, y_initial, step_size):
 
-    time_points = [t_start]
-    solution_points = [y_initial]
+    y_current = np.array(y_initial, dtype=float)
 
     num_steps = int(round((t_end - t_start) / step_size))
 
+    time_points = np.zeros(num_steps + 1)
+    solution_points = np.zeros((num_steps + 1, len(y_current)))
+
+    time_points[0] = t_start
+    solution_points[0] = y_current
+
     t_current = t_start
-    y_current = y_initial
 
     for i in range(1, num_steps + 1):
         k1 = derivative(t_current, y_current)
@@ -19,8 +25,8 @@ def runge_kutta_4(derivative, t_start, t_end, y_initial, step_size):
         y_current = y_current + average_of_derivatives
         t_current = t_start + (i * step_size)
 
-        solution_points.append(y_current)
-        time_points.append(t_current)
+        time_points[i] = t_current
+        solution_points[i] = y_current
 
 
 
